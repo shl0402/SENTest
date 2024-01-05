77f21d2be10539dff509e60712827349830dbca8
pithon <129762334+shl0402@users.noreply.github.com>
2024-01-06 01:14:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 #This module provides a way to interact with the operating system, such as accessing environment variables, working with files
 #and directories, executing shell commands, etc
 import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = ""
 
 # An embedding is a vector (list) of floating point numbers. The distance between two vectors measures their relatedness. 
 # Small distances suggest high relatedness and large distances suggest low relatedness.
